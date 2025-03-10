d66666602d12ca1950f30bd331ac479727961595
Phil <supersmashphil@gmail.com>
2025-03-09 21:15:15 -0600

    cleanup, component cache unit test, resharper formatting added.

--- SmashTools/SmashTools/Performance/Caching/MapComponentCache.cs
+++ SmashTools/SmashTools/Performance/Caching/MapComponentCache.cs
@@ -7,62 +7,83 @@ using RimWorld;
 
 namespace SmashTools
 {
-	[StaticConstructorOnStartup]
-	public static class MapComponentCache
-	{
-		private static readonly List<Type> priorityComponentTypes = new List<Type>();
+  [StaticConstructorOnStartup]
+  public static class MapComponentCache
+  {
+    private static readonly List<Type> priorityComponentTypes;
 
-		static MapComponentCache()
-		{
-			priorityComponentTypes = GenTypes.AllSubclassesNonAbstract(typeof(MapComponent)).ToList();
-		}
+    static MapComponentCache()
+    {
+      priorityComponentTypes = typeof(MapComponent).AllSubclassesNonAbstract().ToList();
+    }
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static T GetCachedMapComponent<T>(this Map map) where T : MapComponent
-		{
-			return MapComponentCache<T>.GetComponent(map);
-		}
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T GetCachedMapComponent<T>(this Map map) where T : MapComponent
+    {
+      return MapComponentCache<T>.GetComponent(map);
+    }
 
-		internal static void ClearMap(Map map)
-		{
-			foreach (Type type in priorityComponentTypes)
-			{
-				GenGeneric.InvokeStaticMethodOnGenericType(typeof(MapComponentCache<>), type, nameof(MapComponentCache<MapComponent>.ClearMap), map);
-			}
-		}
+    internal static void ClearMap(Map map)
+    {
+      foreach (Type type in priorityComponentTypes)
+      {
+        GenGeneric.InvokeStaticMethodOnGenericType(typeof(MapComponentCache<>), type,
+          nameof(MapComponentCache<MapComponent>.ClearMap), map);
+      }
+    }
 
-		internal static void ClearAll()
-		{
-			foreach (Type type in priorityComponentTypes)
-			{
-				GenGeneric.InvokeStaticMethodOnGenericType(typeof(MapComponentCache<>), type, nameof(MapComponentCache<MapComponent>.ClearAll));
-			}
-		}
-	}
+    internal static void ClearAll()
+    {
+      foreach (Type type in priorityComponentTypes)
+      {
+        GenGeneric.InvokeStaticMethodOnGenericType(typeof(MapComponentCache<>), type,
+          nameof(MapComponentCache<MapComponent>.ClearAll));
+      }
+    }
 
-	public static class MapComponentCache<T> where T : MapComponent
-	{
-		public static T[] mapComps = new T[sbyte.MaxValue];
+    internal static int CountAll()
+    {
+      int count = 0;
+      foreach (Type type in priorityComponentTypes)
+      {
+        count += (int)GenGeneric.InvokeStaticMethodOnGenericType(typeof(MapComponentCache<>), type,
+          nameof(MapComponentCache<MapComponent>.Count));
+      }
 
-		public static T GetComponent(Map map)
-		{
-			T component = mapComps[map.Index];
-			if (component == null)
-			{
-				component = map.GetComponent<T>();
-				mapComps[map.Index] = component;
-			}
-			return component;
-		}
+      return count;
+    }
+  }
 
-		public static void ClearMap(Map map)
-		{
-			mapComps[map.Index] = null; //Free up cached component so it can be fetched when index is reused
-		}
+  public static class MapComponentCache<T> where T : MapComponent
+  {
+    private static T[] mapComps = new T[sbyte.MaxValue];
 
-		public static void ClearAll()
-		{
-			mapComps = new T[sbyte.MaxValue];
-		}
-	}
+    public static T GetComponent(Map map)
+    {
+      T component = mapComps[map.Index];
+      if (component == null)
+      {
+        component = map.GetComponent<T>();
+        mapComps[map.Index] = component;
+      }
+
+      return component;
+    }
+
+    public static void ClearMap(Map map)
+    {
+      // Free up cached component so it can be fetched when index is reused
+      mapComps[map.Index] = null;
+    }
+
+    public static void ClearAll()
+    {
+      mapComps = new T[sbyte.MaxValue];
+    }
+
+    internal static int Count()
+    {
+      return mapComps.CountWhere(item => item is not null);
+    }
+  }
 }
--- SmashTools/SmashTools/Utility/Extensions/Collections/Ext_IEnumerable.cs
+++ SmashTools/SmashTools/Utility/Extensions/Collections/Ext_IEnumerable.cs
@@ -7,151 +7,163 @@ using RimWorld;
 
 namespace SmashTools
 {
-	public static class Ext_IEnumerable
-	{
-		/// <summary>
-		/// Random item from those that fit conditions
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="enumerable"></param>
-		/// <param name="predicate"></param>
-		public static T RandomOrDefault<T>(this IEnumerable<T> enumerable, Predicate<T> predicate = null)
-		{
-			if (enumerable.Where(item => predicate is null || predicate(item)).TryRandomElement(out T result))
-			{
-				return result;
-			}
-			return default(T);
-		}
+  public static class Ext_IEnumerable
+  {
+    /// <summary>
+    /// Random item from those that fit conditions
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="enumerable"></param>
+    /// <param name="predicate"></param>
+    public static T RandomOrDefault<T>(this IEnumerable<T> enumerable,
+      Predicate<T> predicate = null)
+    {
+      if (enumerable.Where(item => predicate is null || predicate(item))
+         .TryRandomElement(out T result))
+      {
+        return result;
+      }
 
-		/// <summary>
-		/// Random item from those that fit conditions
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="enumerable"></param>
-		/// <param name="predicate"></param>
-		/// <param name="fallback"></param>
-		public static T RandomOrFallback<T>(this IEnumerable<T> enumerable, Predicate<T> predicate = null, T fallback = default(T))
-		{
-			if (enumerable.Where(item => predicate is null || predicate(item)).TryRandomElement(out T result))
-			{
-				return result;
-			}
-			return fallback;
-		}
+      return default(T);
+    }
 
-		/// <summary>
-		/// .Any extension method but takes into account null collections do not contain the object. 
-		/// Does not throw error on null collections which is more applicable to this project.
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="list"></param>
-		/// <param name="predicate"></param>
-		/// <returns></returns>
-		public static bool NotNullAndAny<T>(this IEnumerable<T> enumerable, Predicate<T> predicate = null)
-		{
-			return enumerable != null && (predicate is null ? enumerable.Any() : enumerable.Any(e => predicate(e)));
-		}
+    /// <summary>
+    /// Random item from those that fit conditions
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="enumerable"></param>
+    /// <param name="predicate"></param>
+    /// <param name="fallback"></param>
+    public static T RandomOrFallback<T>(this IEnumerable<T> enumerable,
+      Predicate<T> predicate = null, T fallback = default(T))
+    {
+      if (enumerable.Where(item => predicate is null || predicate(item))
+         .TryRandomElement(out T result))
+      {
+        return result;
+      }
 
-		public static bool NullOrEmpty<T>(this IEnumerable<T> enumerable)
-		{
-			if (enumerable == null)
-			{
-				return true;
-			}
-			return !enumerable.Any();
-		}
+      return fallback;
+    }
 
-		/// <summary>
-		/// Uncached Count check with conditional predicate
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="list"></param>
-		/// <param name="predicate"></param>
-		public static int CountWhere<T>(this IEnumerable<T> list, Predicate<T> predicate)
-		{
-			int count = 0;
-			foreach (T item in list)
-			{
-				if (predicate(item)) { count++; }
-			}
-			return count;
-		}
+    /// <summary>
+    /// .Any extension method but takes into account null collections do not contain the object. 
+    /// Does not throw error on null collections which is more applicable to this project.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="list"></param>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    public static bool NotNullAndAny<T>(this IEnumerable<T> enumerable,
+      Predicate<T> predicate = null)
+    {
+      return enumerable != null &&
+             (predicate is null ? enumerable.Any() : enumerable.Any(e => predicate(e)));
+    }
 
-		/// <summary>
-		/// Performs the specified action on each element of <c>IEnumerable <typeparamref name="T"/></c>
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="enumerable"></param>
-		/// <param name="action"></param>
-		public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
-		{
-			foreach(T item in enumerable)
-			{
-				action(item);
-			}
-		}
+    public static bool NullOrEmpty<T>(this IEnumerable<T> enumerable)
+    {
+      if (enumerable == null)
+      {
+        return true;
+      }
 
-		/// <summary>
-		/// Check if <paramref name="source"/> is entirely contained within <paramref name="target"/>
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="source"></param>
-		/// <param name="target"></param>
-		public static bool ContainsAllOfList<T>(this IEnumerable<T> source, IEnumerable<T> target)
-		{
-			if (source is null || target is null) return false;
-			return source.Intersect(target).NotNullAndAny();
-		}
+      return !enumerable.Any();
+    }
 
-		/// <summary>
-		/// Creates a RotatingList&lt;<typeparamref name="T"/>&gt; from an IEnumerable&lt;<typeparamref name="T"/>&gt;
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="sourceCollection"></param>
-		public static RotatingList<T> ToRotatingList<T>(this IEnumerable<T> sourceCollection)
-		{
-			return new RotatingList<T>(sourceCollection);
-		}
+    /// <summary>
+    /// Uncached Count check with conditional predicate
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="list"></param>
+    /// <param name="predicate"></param>
+    public static int CountWhere<T>(this IEnumerable<T> list, Predicate<T> predicate)
+    {
+      int count = 0;
+      foreach (T item in list)
+      {
+        if (predicate(item)) count++;
+      }
 
-		/// <summary>
-		/// Join list into readable string
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="enumerable"></param>
-		public static string ToReadableString<T>(this IEnumerable<T> enumerable)
-		{
-			return string.Join(",", enumerable);
-		}
+      return count;
+    }
 
-		/// <summary>
-		/// Prepend multiple objects to enumerable
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="enumerable"></param>
-		/// <param name="items"></param>
-		public static IEnumerable<T> AppendMany<T>(this IEnumerable<T> enumerable, T[] items)
-		{
-			for (int i = items.Count() - 1; i >= 0; i--)
-			{
-				enumerable = enumerable.Append(items[i]);
-			}
-			return enumerable;
-		}
+    /// <summary>
+    /// Performs the specified action on each element of <c>IEnumerable <typeparamref name="T"/></c>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="enumerable"></param>
+    /// <param name="action"></param>
+    public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+    {
+      foreach (T item in enumerable)
+      {
+        action(item);
+      }
+    }
 
-		/// <summary>
-		/// Prepend multiple objects to enumerable
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="enumerable"></param>
-		/// <param name="items"></param>
-		public static IEnumerable<T> PrependMany<T>(this IEnumerable<T> enumerable, T[] items)
-		{
-			for (int i = items.Count() - 1; i >= 0; i--)
-			{
-				enumerable = enumerable.Prepend(items[i]);
-			}
-			return enumerable;
-		}
-	}
+    /// <summary>
+    /// Check if <paramref name="source"/> is entirely contained within <paramref name="target"/>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="source"></param>
+    /// <param name="target"></param>
+    public static bool ContainsAllOfList<T>(this IEnumerable<T> source, IEnumerable<T> target)
+    {
+      if (source is null || target is null) return false;
+      return source.Intersect(target).NotNullAndAny();
+    }
+
+    /// <summary>
+    /// Creates a RotatingList&lt;<typeparamref name="T"/>&gt; from an IEnumerable&lt;<typeparamref name="T"/>&gt;
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="sourceCollection"></param>
+    public static RotatingList<T> ToRotatingList<T>(this IEnumerable<T> sourceCollection)
+    {
+      return new RotatingList<T>(sourceCollection);
+    }
+
+    /// <summary>
+    /// Join list into readable string
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="enumerable"></param>
+    public static string ToReadableString<T>(this IEnumerable<T> enumerable)
+    {
+      return string.Join(",", enumerable);
+    }
+
+    /// <summary>
+    /// Prepend multiple objects to enumerable
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="enumerable"></param>
+    /// <param name="items"></param>
+    public static IEnumerable<T> AppendMany<T>(this IEnumerable<T> enumerable, T[] items)
+    {
+      for (int i = items.Count() - 1; i >= 0; i--)
+      {
+        enumerable = enumerable.Append(items[i]);
+      }
+
+      return enumerable;
+    }
+
+    /// <summary>
+    /// Prepend multiple objects to enumerable
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="enumerable"></param>
+    /// <param name="items"></param>
+    public static IEnumerable<T> PrependMany<T>(this IEnumerable<T> enumerable, T[] items)
+    {
+      for (int i = items.Count() - 1; i >= 0; i--)
+      {
+        enumerable = enumerable.Prepend(items[i]);
+      }
+
+      return enumerable;
+    }
+  }
 }
