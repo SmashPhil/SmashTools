8e2c3199e425e599dc0fe2830240d0379cc19b98
Phil <supersmashphil@gmail.com>
2025-07-01 12:42:43 -0600

    scoped value rollbacks.

---
+++ SmashTools/SmashTools/Utility/ScopedValueRollback.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace SmashTools;
+
+/// <summary>
+/// Records the current value of an object and rolls it back when this struct goes out of scope.
+/// </summary>
+/// <typeparam name="T">Type of the object being rolled back.</typeparam>
+[PublicAPI]
+public readonly unsafe struct ScopedValueRollback<T> : IDisposable where T : unmanaged
+{
+  private readonly T* ptr;
+  private readonly T value;
+
+  public ScopedValueRollback(ref T obj)
+  {
+    fixed (T* objPtr = &obj)
+    {
+      ptr = objPtr;
+      value = obj;
+    }
+  }
+
+  void IDisposable.Dispose()
+  {
+    *ptr = value;
+  }
+}
