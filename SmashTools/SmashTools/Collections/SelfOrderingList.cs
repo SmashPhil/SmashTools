53fa84fbb9b09617823f8a60a569baf91f06664c
Phil <supersmashphil@gmail.com>
2025-07-01 15:24:37 -0600

    cleaned up SelfOrderingList, updated csproj

--- SmashTools/SmashTools/Collections/SelfOrderingList.cs
+++ SmashTools/SmashTools/Collections/SelfOrderingList.cs
@@ -2,17 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 
 namespace SmashTools;
 
 /// <summary>
 /// Self ordering list that pushes most frequently accessed items to the top of the list. 
-/// Increases performance by decreasing full list searches
+/// Increases performance by decreasing frequency of worst case scenarios.
 /// </summary>
-/// <typeparam name="T"></typeparam>
-public class SelfOrderingList<T> : IList<T>, IReadOnlyList<T>
+[PublicAPI]
+public sealed class SelfOrderingList<T> : IList<T>, IReadOnlyList<T>
 {
   private const int DefaultSize = 3;
+
+  private static readonly T[] EmptyContents = [];
+
+  // ReSharper disable once StaticMemberInGenericType
+  private static readonly uint[] EmptyCounters = [];
 
   private T[] contents;
   private uint[] counters;
@@ -21,14 +28,12 @@ public class SelfOrderingList<T> : IList<T>, IReadOnlyList<T>
 
   private int version;
 
-  private static readonly T[] emptyContents = [];
-  private static readonly uint[] emptyCounters = [];
+  private EqualityComparer<T> comparer = EqualityComparer<T>.Default;
 
   public SelfOrderingList()
   {
-    contents = emptyContents;
-    counters = emptyCounters;
-    size = 0;
+    contents = EmptyContents;
+    counters = EmptyCounters;
   }
 
   public SelfOrderingList(int capacity)
@@ -39,31 +44,28 @@ public class SelfOrderingList<T> : IList<T>, IReadOnlyList<T>
 
     if (capacity == 0)
     {
-      contents = emptyContents;
-      counters = emptyCounters;
-      size = 0;
+      contents = EmptyContents;
+      counters = EmptyCounters;
     }
     else
     {
       contents = new T[capacity];
       counters = new uint[capacity];
-      size = capacity;
     }
   }
 
   public SelfOrderingList(IEnumerable<T> enumerable)
   {
     if (enumerable is null)
-      throw new NullReferenceException(
-        "collection cannot be null when initializing a new SelfOrderingList");
+      throw new ArgumentNullException(nameof(enumerable));
 
     if (enumerable is ICollection<T> collection)
     {
       int count = collection.Count;
       if (count == 0)
       {
-        contents = emptyContents;
-        counters = emptyCounters;
+        contents = EmptyContents;
+        counters = EmptyCounters;
       }
       else
       {
@@ -76,8 +78,8 @@ public class SelfOrderingList<T> : IList<T>, IReadOnlyList<T>
     else
     {
       size = 0;
-      contents = emptyContents;
-      counters = emptyCounters;
+      contents = EmptyContents;
+      counters = EmptyCounters;
       foreach (T item in enumerable)
       {
         Add(item);
@@ -95,7 +97,8 @@ public class SelfOrderingList<T> : IList<T>, IReadOnlyList<T>
     set
     {
       if (value < size)
-        throw new ArgumentOutOfRangeException(nameof(value));
+        throw new ArgumentOutOfRangeException(nameof(value),
+          "Setting capacity lower than current item count.");
 
       if (value != contents.Length)
       {
@@ -114,9 +117,10 @@ public class SelfOrderingList<T> : IList<T>, IReadOnlyList<T>
         }
         else
         {
-          contents = emptyContents;
-          counters = emptyCounters;
+          contents = EmptyContents;
+          counters = EmptyCounters;
         }
+        version++;
       }
     }
   }
@@ -125,16 +129,14 @@ public class SelfOrderingList<T> : IList<T>, IReadOnlyList<T>
   {
     get
     {
-      if ((uint)index >= (uint)size)
+      if (index < 0 || index >= size)
         throw new ArgumentOutOfRangeException(nameof(index));
-
       return contents[index];
     }
     set
     {
-      if ((uint)index >= (uint)size)
+      if (index < 0 || index >= size)
         throw new ArgumentOutOfRangeException(nameof(index));
-
       contents[index] = value;
       version++;
     }
@@ -142,15 +144,13 @@ public class SelfOrderingList<T> : IList<T>, IReadOnlyList<T>
 
   private static bool IsCompatibleObject(object value)
   {
-    return value is T || (value is null && default(T) is null);
+    return value is T || value is null && default(T) is null;
   }
 
   public void Add(T item)
   {
     if (size == contents.Length)
-    {
       EnsureCapacity(size + 1);
-    }
 
     contents[size] = item;
     counters[size] = 0;
@@ -169,37 +169,25 @@ public class SelfOrderingList<T> : IList<T>, IReadOnlyList<T>
   public void InsertRange(int index, IEnumerable<T> enumerable)
   {
     if (enumerable is null)
-      throw new ArgumentNullException();
-
-    if ((uint)index > (uint)size)
+      throw new ArgumentNullException(nameof(enumerable));
+    if (index < 0 || index > size)
       throw new ArgumentOutOfRangeException(nameof(index));
+    // ReSharper disable once PossibleUnintendedReferenceComparison
+    if (this == enumerable)
+      throw new NotSupportedException("Self insertion is not supported");
 
     if (enumerable is ICollection<T> collection)
     {
       int count = collection.Count;
-      if (count > 0)
-      {
-        EnsureCapacity(size + count);
-        if (index < size)
-        {
-          Array.Copy(contents, index, contents, index + count, size - index);
-        }
+      if (count == 0)
+        return;
 
-        if (this == collection)
-        {
-          Array.Copy(contents, 0, contents, index, index);
-          Array.Copy(contents, index + count, contents, index * 2, size - index);
-        }
-        else
-        {
-          T[] itemsToInsert = new T[count];
-          collection.CopyTo(itemsToInsert, 0);
-          itemsToInsert.CopyTo(contents, index);
-          counters = new uint[count];
-        }
-
-        size += count;
-      }
+      EnsureCapacity(size + count);
+      Array.Copy(contents, index, contents, index + count, size - index);
+      Array.Copy(counters, index, counters, index + count, size - index);
+      collection.CopyTo(contents, index);
+      size += count;
+      version++;
     }
     else
     {
@@ -208,8 +196,6 @@ public class SelfOrderingList<T> : IList<T>, IReadOnlyList<T>
         Insert(index++, item);
       }
     }
-
-    version++;
   }
 
   public IEnumerator GetEnumerator()
@@ -233,115 +219,89 @@ public class SelfOrderingList<T> : IList<T>, IReadOnlyList<T>
     {
       Array.Clear(contents, 0, size);
       Array.Clear(counters, 0, size);
+      size = 0;
     }
-
     version++;
   }
 
   public bool Contains(T item)
   {
-    if (item is null)
-    {
-      for (int i = 0; i < size; i++)
-      {
-        if (contents[i] is null)
-        {
-          return true;
-        }
-      }
-    }
-    else
-    {
-      EqualityComparer<T> comparer = EqualityComparer<T>.Default;
-      for (int i = 0; i < size; i++)
-      {
-        if (comparer.Equals(contents[i], item))
-        {
-          return true;
-        }
-      }
-    }
-
-    return false;
+    // IndexOf calls Array.IndexOf, which internally calls EqualityComparer<T>.Default.IndexOf,
+    // which is specialized for different types. This boosts performance since instead of making a
+    // virtual method call each iteration of the loop, via EqualityComparer<T>.Default.Equals, we
+    // only make one virtual call to EqualityComparer.IndexOf.
+    return size > 0 && IndexOf(item) >= 0;
   }
 
   private void EnsureCapacity(int min)
   {
+    // Array.MaxArrayLength
+    const uint MaxArrayLength = 2146435071U;
+
     if (contents.Length < min)
     {
-      int newMin = contents.Length == 0 ? DefaultSize : contents.Length * 2;
-
-      if (newMin < min)
-      {
-        newMin = min;
-      }
-
-      Capacity = newMin;
+      int newCapacity = contents.Length == 0 ? DefaultSize : contents.Length * 2;
+      if ((uint)newCapacity > MaxArrayLength)
+        newCapacity = (int)MaxArrayLength;
+      if (newCapacity < min)
+        newCapacity = min;
+      Capacity = newCapacity;
     }
   }
 
-  public T Retrieve(T item)
+  public T Grab(T item)
+  {
+    TryGrab(item, out T result);
+    return result;
+  }
+
+  public bool TryGrab(T item, out T result)
   {
     for (int i = 0; i < size; i++)
     {
-      if (EqualityComparer<T>.Default.Equals(contents[i], item))
-      {
-        counters[i]++;
-
-        if (i > 0 && counters[i] > counters[i - 1])
-        {
-          Bump(i);
-        }
-
-        if (counters[i] >= uint.MaxValue)
-        {
-          ResetCounters();
-        }
-
-        return contents[i];
-      }
+      if (!comparer.Equals(contents[i], item))
+        continue;
+      Touch(i);
+      result = contents[i];
+      return true;
     }
-
-    return default;
+    result = default;
+    return false;
   }
 
-  public void CountIndex(int index)
+  public void Touch(int index)
   {
-    if ((uint)index >= (uint)size)
+    if (index < 0 || index >= size)
       throw new ArgumentOutOfRangeException(nameof(index));
 
     counters[index]++;
-    if (index > 0 && counters[index] > counters[index - 1])
-    {
+    if (counters[index] > counters[index - 1])
       Bump(index);
-    }
-
-    if (counters[index] >= uint.MaxValue)
-    {
+    if (counters[index] == uint.MaxValue)
       ResetCounters();
-    }
   }
 
-  public void ResetCounters()
+  private void ResetCounters()
   {
+    // Preserve relative ranking so items aren't suddenly bumped around after a reset
     for (int i = 0; i < size; i++)
     {
-      counters[i] = 0;
+      counters[i] = (uint)(size - i);
     }
   }
 
+  [Pure]
   public int IndexOf(T item)
   {
     return Array.IndexOf(contents, item, 0, size);
   }
 
-  public void Bump(int index)
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  private void Bump(int index)
   {
-    if ((uint)index <= 0 || (uint)index > (uint)size)
-      throw new ArgumentOutOfRangeException(nameof(index));
-
     // Destruction swap
     (contents[index], contents[index - 1]) = (contents[index - 1], contents[index]);
+    (counters[index], counters[index - 1]) = (counters[index - 1], counters[index]);
     version++;
   }
 
@@ -351,14 +311,10 @@ public class SelfOrderingList<T> : IList<T>, IReadOnlyList<T>
       throw new ArgumentOutOfRangeException(nameof(index));
 
     if (size == contents.Length)
-    {
       EnsureCapacity(size + 1);
-    }
 
     if (index < size)
-    {
       Array.Copy(contents, index, contents, index + 1, size - index);
-    }
 
     contents[index] = item;
     size++;
@@ -379,16 +335,14 @@ public class SelfOrderingList<T> : IList<T>, IReadOnlyList<T>
 
   public void RemoveAt(int index)
   {
-    if ((uint)index >= (uint)size)
+    if (index < 0 || index >= size)
       throw new ArgumentOutOfRangeException(nameof(index));
 
     size--;
-    if (index < size)
-    {
-      Array.Copy(contents, index + 1, contents, index, size - index);
-    }
-
-    contents[size] = default;
+    Array.Copy(contents, index + 1, contents, index, size - index);
+    Array.Copy(counters, index + 1, counters, index, size - index);
+    contents[size] = default!;
+    counters[size] = 0;
     version++;
   }
 
@@ -399,6 +353,10 @@ public class SelfOrderingList<T> : IList<T>, IReadOnlyList<T>
 
   public void CopyTo(T[] array, int arrayIndex)
   {
+    if (array != null && array.Rank != 1)
+      throw new ArgumentException("Multi-rank dimensions are not supported for SelfOrderingList",
+        nameof(array));
+    // Array.Copy checks for null
     Array.Copy(contents, 0, array, arrayIndex, size);
   }
 
