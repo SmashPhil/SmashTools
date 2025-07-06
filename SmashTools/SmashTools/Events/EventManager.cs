37bb9de6a57835d6145496b9880ac315690391d7
Phil <supersmashphil@gmail.com>
2025-07-06 10:13:26 -0600

    xml documentation, unit test, and cleanup for event manager and trigger implementations.

--- SmashTools/SmashTools/Events/EventManager.cs
+++ SmashTools/SmashTools/Events/EventManager.cs
@@ -1,28 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Verse;
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
 
-namespace SmashTools
+namespace SmashTools;
+
+/// <summary>
+/// Maintains a mapping from event keys of type <typeparamref name="T"/> to their <see cref="EventTrigger"/> instances.
+/// Allows enabling/disabling of all triggers and lazy initialization of triggers per key.
+/// </summary>
+/// <typeparam name="T">The type used as the event key.</typeparam>
+[PublicAPI]
+public class EventManager<T> : IEventControl
 {
-  public class EventManager<T>
+  // Backing field for IEventEnabler::Enabled
+  private bool enabled = true;
+
+  /// <summary>
+  /// Internal storage for event triggers keyed by <typeparamref name="T"/>.
+  /// </summary>
+  public readonly Dictionary<T, EventTrigger> map = [];
+
+  /// <summary>
+  /// Gets or sets whether all events in this manager are enabled. When <see langword="false"/>, no callbacks will execute.
+  /// </summary>
+  public bool Enabled
   {
-    public readonly Dictionary<T, EventTrigger> lookup = [];
+    get => enabled;
+    private set => enabled = value;
+  }
 
-    public bool Enabled { get; set; } = true;
+  /// <summary>
+  /// Gets or sets whether all events in this manager are enabled. When <see langword="false"/>, no callbacks will execute.
+  /// </summary>
+  bool IEventControl.Enabled
+  {
+    get => enabled;
+    set => enabled = value;
+  }
 
-    public EventTrigger this[T key]
+  /// <summary>
+  /// Gets or sets the <see cref="EventTrigger"/> for the specified key, creating a new one if none exists.
+  /// </summary>
+  /// <param name="key">The event key to retrieve or assign a trigger for.</param>
+  /// <returns>The <see cref="EventTrigger"/> associated with <paramref name="key"/>.</returns>
+  public EventTrigger this[T key]
+  {
+    get
     {
-      get
-      {
-        if (!lookup.ContainsKey(key))
-        {
-          lookup[key] = new EventTrigger();
-        }
-
-        return lookup[key];
-      }
-      set { lookup[key] = value; }
+      if (!map.ContainsKey(key))
+        map[key] = new EventTrigger(this);
+      return map[key];
     }
+    set { map[key] = value; }
   }
 }
---
+++ SmashTools/SmashTools/Events/IEventControl.cs
@@ -0,0 +1,14 @@
+﻿namespace SmashTools;
+
+/// <summary>
+/// Provides control over the enabled state of an event manager or trigger.
+/// </summary>
+public interface IEventControl
+{
+  /// <summary>
+  /// Gets or sets a value indicating whether events are permitted to execute.
+  /// When <see langword="true"/>, event processing is allowed; when <see langword="false"/>,
+  /// event execution should be suppressed.
+  /// </summary>
+  public bool Enabled { get; set; }
+}
