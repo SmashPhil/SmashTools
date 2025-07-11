15ba282d6556ab471a59f8aedbc4f41d5c806beb
Phil <supersmashphil@gmail.com>
2025-07-11 00:02:08 -0600

    enum extension for min / max.

--- SmashTools/SmashTools/Utility/Extensions/Game/Ext_Comp.cs
+++ SmashTools/SmashTools/Utility/Extensions/Game/Ext_Comp.cs
@@ -1,59 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection;
 using HarmonyLib;
 using JetBrains.Annotations;
 using Verse;
 
-namespace SmashTools
+namespace SmashTools;
+
+public static class Ext_Comp
 {
-  public static class Ext_Comp
+  private static readonly AccessTools.FieldRef<ThingWithComps, List<ThingComp>> CompList =
+    AccessTools.FieldRefAccess<ThingWithComps, List<ThingComp>>("comps");
+
+  /// <summary>
+  /// Adds <paramref name="comp"/> to <paramref name="thingWithComps"/> and inits inner 'comps' list if empty.
+  /// </summary>
+  /// <typeparam name="T"></typeparam>
+  /// <param name="thingWithComps"></param>
+  /// <param name="comp"></param>
+  [Pure]
+  public static bool TryAddComp<T>(this ThingWithComps thingWithComps, T comp) where T : ThingComp
   {
-    private static readonly FieldInfo compList = AccessTools.Field(typeof(ThingWithComps), "comps");
-
-    /// <summary>
-    /// Adds <paramref name="comp"/> to <paramref name="thingWithComps"/> and inits inner 'comps' list if empty.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="thingWithComps"></param>
-    /// <param name="comp"></param>
-    [Pure]
-    public static bool TryAddComp<T>(this ThingWithComps thingWithComps, T comp) where T : ThingComp
+    try
     {
-      try
-      {
-        thingWithComps.EnsureUncachedCompList();
-        thingWithComps.AllComps.Add(comp);
-        return true;
-      }
-      catch (Exception ex)
-      {
-        SmashLog.Error(
-          $"Exception thrown while trying to reflectively add <type>{comp.GetType()}</type> to {thingWithComps}.\nException={ex}");
-      }
-      return false;
+      thingWithComps.EnsureUncachedCompList();
+      thingWithComps.AllComps.Add(comp);
+      return true;
     }
-
-    /// <summary>
-    /// Assigns new list reference to comps field of <paramref name="thingWithComps"/>, rather than using an empty static list shared among all instances
-    /// </summary>
-    public static bool EnsureUncachedCompList(this ThingWithComps thingWithComps)
+    catch (Exception ex)
     {
-      try
-      {
-        var compListInstance = (List<ThingComp>)compList.GetValue(thingWithComps);
-        if (compListInstance == null)
-        {
-          compList.SetValue(thingWithComps, new List<ThingComp>());
-          return true;
-        }
-      }
-      catch (Exception ex)
-      {
-        SmashLog.Error(
-          $"Exception thrown while trying to uncache compList for {thingWithComps}.\nException={ex}");
-      }
-      return false;
+      SmashLog.Error(
+        $"Exception thrown while trying to reflectively add <type>{comp.GetType()}</type> to {thingWithComps}.\nException={ex}");
+    }
+    return false;
+  }
+
+  /// <summary>
+  /// Assigns new list reference to comps field of <paramref name="thingWithComps"/>, rather than using an empty static list shared among all instances
+  /// </summary>
+  private static void EnsureUncachedCompList(this ThingWithComps thingWithComps)
+  {
+    try
+    {
+      CompList.Invoke(instance: thingWithComps) ??= [];
+    }
+    catch (Exception ex)
+    {
+      SmashLog.Error(
+        $"Exception thrown while trying to uncache compList for {thingWithComps}.\nException={ex}");
     }
   }
 }
---
+++ SmashTools/SmashTools/Utility/Extensions/System/Ext_Enum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace SmashTools;
+
+/// <summary>
+/// Utility methods for <see cref="Enum"/> types.
+/// </summary>
+[PublicAPI]
+public static class Ext_Enum
+{
+  /// <summary>
+  /// Returns the lesser of two enum values using the default comparer.
+  /// </summary>
+  /// <typeparam name="T">The enum type being compared.</typeparam>
+  /// <param name="a">The first value to compare.</param>
+  /// <param name="b">The second value to compare.</param>
+  /// <returns>
+  /// <paramref name="a"/> if it is less than or equal to <paramref name="b"/>; otherwise <paramref name="b"/>.
+  /// </returns>
+  public static T Min<T>(T a, T b) where T : Enum
+  {
+    return Comparer<T>.Default.Compare(a, b) <= 0 ? a : b;
+  }
+
+  /// <summary>
+  /// Returns the greater of two enum values using the default comparer.
+  /// </summary>
+  /// <typeparam name="T">The enum type being compared.</typeparam>
+  /// <param name="a">The first value to compare.</param>
+  /// <param name="b">The second value to compare.</param>
+  /// <returns>
+  /// <paramref name="a"/> if it is greater than or equal to <paramref name="b"/>; otherwise <paramref name="b"/>.
+  /// </returns>
+  public static T Max<T>(T a, T b) where T : Enum
+  {
+    return Comparer<T>.Default.Compare(a, b) >= 0 ? a : b;
+  }
+}
