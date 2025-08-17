bbd342b8c4ece62442ce8b5602334cad10c079a2
Phil <supersmashphil@gmail.com>
2025-08-16 21:39:22 -0600

    removed unsafe extension for bit flag check.

--- SmashTools/SmashTools/Utility/Extensions/System/Ext_Enum.cs
+++ SmashTools/SmashTools/Utility/Extensions/System/Ext_Enum.cs
@@ -12,54 +12,55 @@ namespace SmashTools;
 [PublicAPI]
 public static class Ext_Enum
 {
-  /// <summary>
-  /// Returns the lesser of two enum values using the default comparer.
-  /// </summary>
-  /// <typeparam name="T">The enum type being compared.</typeparam>
-  /// <param name="a">The first value to compare.</param>
-  /// <param name="b">The second value to compare.</param>
-  /// <returns>
-  /// <paramref name="a"/> if it is less than or equal to <paramref name="b"/>; otherwise <paramref name="b"/>.
-  /// </returns>
-  public static T Min<T>(T a, T b) where T : Enum
-  {
-    return Comparer<T>.Default.Compare(a, b) <= 0 ? a : b;
-  }
+	/// <summary>
+	/// Returns the lesser of two enum values using the default comparer.
+	/// </summary>
+	/// <typeparam name="T">The enum type being compared.</typeparam>
+	/// <param name="a">The first value to compare.</param>
+	/// <param name="b">The second value to compare.</param>
+	/// <returns>
+	/// <paramref name="a"/> if it is less than or equal to <paramref name="b"/>; otherwise <paramref name="b"/>.
+	/// </returns>
+	public static T Min<T>(T a, T b) where T : Enum
+	{
+		return Comparer<T>.Default.Compare(a, b) <= 0 ? a : b;
+	}
 
-  /// <summary>
-  /// Returns the greater of two enum values using the default comparer.
-  /// </summary>
-  /// <typeparam name="T">The enum type being compared.</typeparam>
-  /// <param name="a">The first value to compare.</param>
-  /// <param name="b">The second value to compare.</param>
-  /// <returns>
-  /// <paramref name="a"/> if it is greater than or equal to <paramref name="b"/>; otherwise <paramref name="b"/>.
-  /// </returns>
-  public static T Max<T>(T a, T b) where T : Enum
-  {
-    return Comparer<T>.Default.Compare(a, b) >= 0 ? a : b;
-  }
+	/// <summary>
+	/// Returns the greater of two enum values using the default comparer.
+	/// </summary>
+	/// <typeparam name="T">The enum type being compared.</typeparam>
+	/// <param name="a">The first value to compare.</param>
+	/// <param name="b">The second value to compare.</param>
+	/// <returns>
+	/// <paramref name="a"/> if it is greater than or equal to <paramref name="b"/>; otherwise <paramref name="b"/>.
+	/// </returns>
+	public static T Max<T>(T a, T b) where T : Enum
+	{
+		return Comparer<T>.Default.Compare(a, b) >= 0 ? a : b;
+	}
 
-  [MethodImpl(MethodImplOptions.AggressiveInlining)]
-  public static unsafe bool IsAnyBitSet<T>(this T a, T b) where T : unmanaged, Enum
-  {
+	// Not used in any meaningful capacity, just a proof of concept
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static unsafe bool IsAnyBitSet<T>(this T a, T b) where T : unmanaged, Enum
+	{
 #if DEBUG
-    if (typeof(T).GetEnumUnderlyingType() != typeof(int))
-      throw new ArgumentException($"{typeof(T).Name} must have an underlying int32 type.");
+		if (typeof(T).GetEnumUnderlyingType() != typeof(int))
+			throw new ArgumentException($"{typeof(T).Name} must have an underlying int32 type.");
 #endif
 
-    return (*(int*)&a & *(int*)&b) != 0;
-  }
+		return (*(int*)&a & *(int*)&b) != 0;
+	}
 
-  [MethodImpl(MethodImplOptions.AggressiveInlining)]
-  public static unsafe bool AreAllBitsSet<T>(this T a, T b) where T : unmanaged, Enum
-  {
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static unsafe bool AreAllBitsSet<T>(this T a, T b) where T : unmanaged, Enum
+	{
 #if DEBUG
-    if (typeof(T).GetEnumUnderlyingType() != typeof(int))
-      throw new ArgumentException($"{typeof(T).Name} must have an underlying int32 type.");
+		if (typeof(T).GetEnumUnderlyingType() != typeof(int))
+			throw new ArgumentException($"{typeof(T).Name} must have an underlying int32 type.");
 #endif
 
-    int bInt = *(int*)&b;
-    return (*(int*)&a & bInt) == bInt;
-  }
+		int bInt = *(int*)&b;
+		return (*(int*)&a & bInt) == bInt;
+	}
 }
