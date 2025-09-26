f5f3c0fc7f78d6e887fa3c77c3265a34d1b277df
Phil <supersmashphil@gmail.com>
2025-09-25 23:55:58 -0600

    added rand state block for unseeded state push.

---
+++ SmashTools/SmashTools/Utility/Rollbacks/RandStateBlock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using JetBrains.Annotations;
+using Verse;
+
+namespace SmashTools;
+
+/// <summary>
+/// RNG state block for RimWorld (unseeded).
+/// </summary>
+/// <remarks>Because Ludeon didn't add this to the default constructor of <see cref="RandBlock"/>.</remarks>
+[PublicAPI]
+public readonly struct RandStateBlock : IDisposable
+{
+	/// <summary>Pushes current <see cref="Verse.Rand"/> state.</summary>
+	public RandStateBlock()
+	{
+		Rand.PushState();
+	}
+
+	/// <summary>Restores the previously pushed <see cref="Verse.Rand"/> state.</summary>
+	void IDisposable.Dispose()
+	{
+		Rand.PopState();
+	}
+}
