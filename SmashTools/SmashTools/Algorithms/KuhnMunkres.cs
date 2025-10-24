2d460b8e93a8bafb9e7db4d52f046c15f461fc19
Phil <supersmashphil@gmail.com>
2025-10-23 18:49:22 -0600

    Renamed burst lib loader.

--- SmashTools/SmashTools/Algorithms/KuhnMunkres.cs
+++ SmashTools/SmashTools/Algorithms/KuhnMunkres.cs
@@ -8,140 +8,143 @@ namespace SmashTools.Algorithms;
 /// </summary>
 public sealed class KuhnMunkres
 {
-  // Square matrix dimension
-  private readonly int n;
+	// Square matrix dimension
+	private readonly int n;
 
-  private readonly float[] labelByWorker;
-  private readonly float[] labelByJob;
-  private readonly int[] minSlackWorkerByJob;
-  private readonly float[] minSlackValueByJob;
-  private readonly int[] matchWorkerByJob;
-  private readonly int[] parentWorkerByCommittedJob;
-  private readonly bool[] committedWorkers;
+	private readonly float[] labelByWorker;
+	private readonly float[] labelByJob;
+	private readonly int[] minSlackWorkerByJob;
+	private readonly float[] minSlackValueByJob;
+	private readonly int[] matchWorkerByJob;
+	private readonly int[] parentWorkerByCommittedJob;
+	private readonly bool[] committedWorkers;
 
-  public KuhnMunkres(int n)
-  {
-    this.n = n;
+	/// <summary>
+	/// Initialize a KuhnMunkres matrix solver, distributing <paramref name="n"/> workers to <paramref name="n"/> potential jobs
+	/// </summary>
+	/// <param name="n">Number of workers to distribute.</param>
+	public KuhnMunkres(int n)
+	{
+		this.n = n;
 
-    labelByWorker = new float[n];
-    labelByJob = new float[n];
-    minSlackWorkerByJob = new int[n];
-    minSlackValueByJob = new float[n];
-    matchWorkerByJob = new int[n];
-    parentWorkerByCommittedJob = new int[n];
-    committedWorkers = new bool[n];
-  }
+		labelByWorker = new float[n];
+		labelByJob = new float[n];
+		minSlackWorkerByJob = new int[n];
+		minSlackValueByJob = new float[n];
+		matchWorkerByJob = new int[n];
+		parentWorkerByCommittedJob = new int[n];
+		committedWorkers = new bool[n];
+	}
 
-  public int[] Compute(float[,] costMatrix)
-  {
-    Assert.AreEqual(costMatrix.GetLength(0), costMatrix.GetLength(1));
-    Assert.AreEqual(costMatrix.GetLength(0), n);
+	public int[] Compute(float[,] costMatrix)
+	{
+		Assert.AreEqual(costMatrix.GetLength(0), costMatrix.GetLength(1));
+		Assert.AreEqual(costMatrix.GetLength(0), n);
 
-    int[] result = new int[n];
-    result.Populate(-1);
-    matchWorkerByJob.Populate(-1);
+		int[] result = new int[n];
+		result.Populate(-1);
+		matchWorkerByJob.Populate(-1);
 
-    InitializeLabels(costMatrix);
+		InitializeLabels(costMatrix);
 
-    for (int i = 0; i < n; i++)
-    {
-      // Only execute on indices that still need processing
-      if (result[i] >= 0)
-        continue;
+		for (int i = 0; i < n; i++)
+		{
+			// Only execute on indices that still need processing
+			if (result[i] >= 0)
+				continue;
 
-      StartPhase(i, costMatrix);
-      ExecutePhase(costMatrix, result);
-    }
-    return result;
-  }
+			StartPhase(i, costMatrix);
+			ExecutePhase(costMatrix, result);
+		}
+		return result;
+	}
 
-  private void InitializeLabels(float[,] costMatrix)
-  {
-    for (int i = 0; i < n; i++)
-    {
-      labelByWorker[i] = float.MaxValue;
-      for (int j = 0; j < n; j++)
-        labelByWorker[i] = Mathf.Min(labelByWorker[i], costMatrix[i, j]);
-    }
-    labelByJob.Populate(0);
-  }
+	private void InitializeLabels(float[,] costMatrix)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			labelByWorker[i] = float.MaxValue;
+			for (int j = 0; j < n; j++)
+				labelByWorker[i] = Mathf.Min(labelByWorker[i], costMatrix[i, j]);
+		}
+	}
 
-  private void StartPhase(int i, float[,] costMatrix)
-  {
-    committedWorkers.Populate(false);
-    parentWorkerByCommittedJob.Populate(-1);
-    committedWorkers[i] = true;
+	private void StartPhase(int i, float[,] costMatrix)
+	{
+		committedWorkers.Populate(false);
+		parentWorkerByCommittedJob.Populate(-1);
+		committedWorkers[i] = true;
 
-    for (int j = 0; j < n; j++)
-    {
-      minSlackValueByJob[j] = costMatrix[i, j] - labelByWorker[i] - labelByJob[j];
-      minSlackWorkerByJob[j] = i;
-    }
-  }
+		for (int j = 0; j < n; j++)
+		{
+			minSlackValueByJob[j] = costMatrix[i, j] - labelByWorker[i] - labelByJob[j];
+			minSlackWorkerByJob[j] = i;
+		}
+	}
 
-  private void ExecutePhase(float[,] costMatrix, int[] result)
-  {
-    while (true)
-    {
-      float minSlackValue = float.MaxValue;
-      int minSlackWorker = -1, minSlackJob = -1;
+	private void ExecutePhase(float[,] costMatrix, int[] result)
+	{
+		while (true)
+		{
+			float minSlackValue = float.MaxValue;
+			int minSlackWorker = -1, minSlackJob = -1;
 
-      for (int j = 0; j < n; j++)
-      {
-        if (parentWorkerByCommittedJob[j] == -1 && minSlackValueByJob[j] < minSlackValue)
-        {
-          minSlackValue = minSlackValueByJob[j];
-          minSlackWorker = minSlackWorkerByJob[j];
-          minSlackJob = j;
-        }
-      }
+			for (int j = 0; j < n; j++)
+			{
+				if (parentWorkerByCommittedJob[j] == -1 && minSlackValueByJob[j] < minSlackValue)
+				{
+					minSlackValue = minSlackValueByJob[j];
+					minSlackWorker = minSlackWorkerByJob[j];
+					minSlackJob = j;
+				}
+			}
 
-      for (int j = 0; j < n; j++)
-      {
-        if (committedWorkers[j])
-          labelByWorker[j] += minSlackValue;
-      }
+			for (int j = 0; j < n; j++)
+			{
+				if (committedWorkers[j])
+					labelByWorker[j] += minSlackValue;
+			}
 
-      for (int j = 0; j < n; j++)
-      {
-        if (parentWorkerByCommittedJob[j] != -1)
-          labelByJob[j] -= minSlackValue;
-        else
-          minSlackValueByJob[j] -= minSlackValue;
-      }
+			for (int j = 0; j < n; j++)
+			{
+				if (parentWorkerByCommittedJob[j] != -1)
+					labelByJob[j] -= minSlackValue;
+				else
+					minSlackValueByJob[j] -= minSlackValue;
+			}
 
-      parentWorkerByCommittedJob[minSlackJob] = minSlackWorker;
+			parentWorkerByCommittedJob[minSlackJob] = minSlackWorker;
 
-      if (matchWorkerByJob[minSlackJob] == -1)
-      {
-        int parentWorker = parentWorkerByCommittedJob[minSlackJob];
-        while (true)
-        {
-          int temp = result[parentWorker];
-          result[parentWorker] = minSlackJob;
-          matchWorkerByJob[minSlackJob] = parentWorker;
-          minSlackJob = temp;
-          if (minSlackJob == -1)
-            break;
-          parentWorker = parentWorkerByCommittedJob[minSlackJob];
-        }
-        break;
-      }
+			if (matchWorkerByJob[minSlackJob] == -1)
+			{
+				int parentWorker = parentWorkerByCommittedJob[minSlackJob];
+				while (true)
+				{
+					int temp = result[parentWorker];
+					result[parentWorker] = minSlackJob;
+					matchWorkerByJob[minSlackJob] = parentWorker;
+					minSlackJob = temp;
+					if (minSlackJob == -1)
+						break;
+					parentWorker = parentWorkerByCommittedJob[minSlackJob];
+				}
+				break;
+			}
 
-      int worker = matchWorkerByJob[minSlackJob];
-      committedWorkers[worker] = true;
-      for (int j = 0; j < n; j++)
-      {
-        if (parentWorkerByCommittedJob[j] != -1)
-          continue;
+			int worker = matchWorkerByJob[minSlackJob];
+			committedWorkers[worker] = true;
+			for (int j = 0; j < n; j++)
+			{
+				if (parentWorkerByCommittedJob[j] != -1)
+					continue;
 
-        float slack = costMatrix[worker, j] - labelByWorker[worker] - labelByJob[j];
-        if (minSlackValueByJob[j] > slack)
-        {
-          minSlackValueByJob[j] = slack;
-          minSlackWorkerByJob[j] = worker;
-        }
-      }
-    }
-  }
+				float slack = costMatrix[worker, j] - labelByWorker[worker] - labelByJob[j];
+				if (minSlackValueByJob[j] > slack)
+				{
+					minSlackValueByJob[j] = slack;
+					minSlackWorkerByJob[j] = worker;
+				}
+			}
+		}
+	}
 }
---
+++ SmashTools/SmashTools/Burst/BurstAssemblyLoader.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using JetBrains.Annotations;
+using Unity.Burst;
+using UnityEngine;
+using UnityEngine.Assertions;
+using Verse;
+
+namespace SmashTools.Burst;
+
+[PublicAPI]
+public static class BurstAssemblyLoader
+{
+	private const string AssemblyFolder = "Burst";
+
+	private const string FolderNameWindows = "Windows";
+	private const string FolderNameLinux = "Linux";
+	private const string FolderNameMac = "Mac";
+	
+	private const string FolderName64 = "x86_64";
+	private const string FolderName32 = "x86";
+	private const string FolderNameArm = "arm64";
+
+	// TODO - I don't think Unity supports 32bit ARM but I can't find any documentation on this.  Will check later
+	private static bool IsArmArchitecture => 
+		RuntimeInformation.ProcessArchitecture is Architecture.Arm64;
+
+	private static string PlatformFolder
+	{
+		get
+		{
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				return Path.Combine(FolderNameWindows, UnityData.Is64BitBuild ? FolderName64 : FolderName32);
+			}
+			else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			{
+				return FolderNameLinux;
+			}
+			else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			{
+				return Path.Combine(FolderNameMac, IsArmArchitecture ? FolderNameArm : FolderName64);
+			}
+			Log.Warning(
+				$"{RuntimeInformation.OSDescription} is not currently supported for burst libraries.");
+			return null;
+		}
+	}
+
+	private static bool IsValidFileExtension(string extension)
+	{
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			return extension == ".dll";
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			return extension == ".so";
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			return extension == ".bundle" || extension == ".dylib";
+
+		return false;
+	}
+
+	/// <summary>
+	/// Loads burst compiled assemblies at runtime.
+	/// </summary>
+	/// <param name="mod">Mod to load burst assemblies for.</param>
+	public static bool LoadAllFor(ModContentPack mod)
+	{
+		string folderName = PlatformFolder;
+		if (folderName.NullOrEmpty())
+			return false;
+
+		bool anyLoaded = false;
+		string folderPath = Path.Combine(AssemblyFolder, folderName);
+		List<string> folders = mod.foldersToLoadDescendingOrder;
+		for (int i = folders.Count; --i >= 0;)
+		{
+			string folder = folders[i];
+			DirectoryInfo directoryInfo = new(Path.Combine(folder, folderPath));
+			if (directoryInfo.Exists)
+			{
+				foreach (FileInfo fileInfo in directoryInfo.EnumerateFiles("*.*", SearchOption.AllDirectories))
+				{
+					try
+					{
+						Assert.IsTrue(IsValidFileExtension(fileInfo.Extension.ToLowerInvariant()));
+						if (!BurstRuntime.LoadAdditionalLibrary(fileInfo.FullName))
+						{
+							Log.Error($"Unable to load file {fileInfo.Name} into BurstRuntime.");
+							return false;
+						}
+						anyLoaded = true;
+					}
+					catch (Exception ex)
+					{
+						Log.Error($"Exception thrown loading {fileInfo.Name} into BurstRuntime.\n{ex}");
+					}
+				}
+			}
+		}
+		return anyLoaded;
+	}
+}
