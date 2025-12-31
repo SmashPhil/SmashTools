84516296eaa6f75d328331fc76763609713bc005
Phillip <supersmashphil@gmail.com>
2025-12-30 23:57:08 -0700

    Move burst methods to osutils. Update Harmony version to 2.4.2

---
+++ Core/Utils/OSUtils.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+using JetBrains.Annotations;
+
+namespace SmashTools.Burst;
+
+[PublicAPI]
+public static class OSUtils
+{
+  public const string BurstFolder = "Burst";
+
+  private const string FolderNameWindows = "Windows";
+  private const string FolderNameLinux = "Linux";
+  private const string FolderNameMac = "OSX";
+
+  private static bool IsArmArchitecture =>
+    RuntimeInformation.ProcessArchitecture is Architecture.Arm64;
+
+  public static OSPlatform GetOSPlatform()
+  {
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+      return OSPlatform.Windows;
+
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+      return OSPlatform.Linux;
+
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+      return OSPlatform.OSX;
+
+    Logger.Error("Platform not detected.");
+    return default;
+  }
+
+  public static string GetPlatformFolder(OSPlatform platform)
+  {
+    if (platform == OSPlatform.Windows)
+      return FolderNameWindows;
+
+    if (platform == OSPlatform.Linux)
+      return FolderNameLinux;
+
+    if (platform == OSPlatform.OSX)
+      return FolderNameMac;
+
+    return null;
+  }
+}
--- SmashTools/SmashTools/Burst/BurstAssemblyLoader.cs
+++ SmashTools/SmashTools/Burst/BurstAssemblyLoader.cs
@@ -6,77 +6,40 @@ using JetBrains.Annotations;
 using Unity.Burst;
 using UnityEngine.Assertions;
 using Verse;
+using static SmashTools.Burst.OSUtils;
 
 namespace SmashTools.Burst;
 
 [PublicAPI]
 public static class BurstAssemblyLoader
 {
-	private const string AssemblyFolder = "Burst";
+  /// <summary>
+  /// Loads burst compiled assemblies at runtime.
+  /// </summary>
+  /// <param name="mod">Mod to load burst assemblies for.</param>
+  public static bool LoadAllFor(ModContentPack mod)
+  {
+    // Not supported for 32bit builds
+    if (!UnityData.Is64BitBuild)
+      return false;
 
-	private const string FolderNameWindows = "Windows";
-	private const string FolderNameLinux = "Linux";
-	private const string FolderNameMac = "Mac";
-	
-	private const string FolderName64 = "x86_64";
-	private const string FolderName32 = "x86";
-	private const string FolderNameArm = "arm64";
-
-	// TODO - I don't think Unity supports 32bit ARM, but I can't find any documentation on this.  Will check later
-	private static bool IsArmArchitecture => 
-		RuntimeInformation.ProcessArchitecture is Architecture.Arm64;
-
-	private static string PlatformFolder
-	{
-		get
-		{
-			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        return System.IO.Path.Combine(FolderNameWindows, UnityData.Is64BitBuild ? FolderName64 : FolderName32);
-
-      if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-        return FolderNameLinux;
-
-      if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-        return System.IO.Path.Combine(FolderNameMac, IsArmArchitecture ? FolderNameArm : FolderName64);
-
+    string folderName = GetPlatformFolder(GetOSPlatform());
+    if (folderName.NullOrEmpty())
+    {
       if (Prefs.DevMode)
       {
-        Log.Message(
-          $"{RuntimeInformation.OSDescription} is not currently supported for burst lib.");
+        Log.Warning($"{RuntimeInformation.OSDescription} is not supported for burst lib.");
       }
-			return null;
-		}
-	}
+      return false;
+    }
 
-	private static bool IsValidFileExtension(string extension)
-	{
-		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-			return extension == ".dll";
-		if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-			return extension == ".so";
-		if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-			return extension == ".bundle" || extension == ".dylib";
-
-		return false;
-	}
-
-	/// <summary>
-	/// Loads burst compiled assemblies at runtime.
-	/// </summary>
-	/// <param name="mod">Mod to load burst assemblies for.</param>
-	public static bool LoadAllFor(ModContentPack mod)
-	{
-		string folderName = PlatformFolder;
-		if (folderName.NullOrEmpty())
-			return false;
-
-		bool anyLoaded = false;
-		string folderPath = System.IO.Path.Combine(AssemblyFolder, folderName);
-		List<string> folders = mod.foldersToLoadDescendingOrder;
-		for (int i = folders.Count; --i >= 0;)
-		{
-			string folder = folders[i];
-			DirectoryInfo directoryInfo = new(System.IO.Path.Combine(folder, folderPath));
+    bool anyLoaded = false;
+    string folderPath = System.IO.Path.Combine(BurstFolder, folderName);
+    List<string> folders = mod.foldersToLoadDescendingOrder;
+    for (int i = folders.Count; --i >= 0;)
+    {
+      string folder = folders[i];
+      DirectoryInfo directoryInfo = new(System.IO.Path.Combine(folder, folderPath));
       if (!directoryInfo.Exists)
         continue;
 
@@ -102,6 +65,18 @@ public static class BurstAssemblyLoader
         }
       }
     }
-		return anyLoaded;
-	}
+    return anyLoaded;
+  }
+
+  public static bool IsValidFileExtension(string extension)
+  {
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+      return extension == ".dll";
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+      return extension == ".so";
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+      return extension == ".bundle";
+
+    return false;
+  }
 }
