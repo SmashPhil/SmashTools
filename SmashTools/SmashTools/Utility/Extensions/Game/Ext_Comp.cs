dca5929133c455909d001d9e4e472ee29f6ac62b
Phillip <supersmashphil@gmail.com>
2026-05-27 18:08:14 -0600

    Add insert comp variant for inserting frequently accessed comps to the top of the list.

--- SmashTools/SmashTools/Utility/Extensions/Game/Ext_Comp.cs
+++ SmashTools/SmashTools/Utility/Extensions/Game/Ext_Comp.cs
@@ -6,6 +6,7 @@ using Verse;
 
 namespace SmashTools;
 
+[PublicAPI]
 public static class Ext_Comp
 {
   private static readonly AccessTools.FieldRef<ThingWithComps, List<ThingComp>> CompList =
@@ -14,39 +15,38 @@ public static class Ext_Comp
   /// <summary>
   /// Adds <paramref name="comp"/> to <paramref name="thingWithComps"/> and inits inner 'comps' list if empty.
   /// </summary>
-  /// <typeparam name="T"></typeparam>
-  /// <param name="thingWithComps"></param>
-  /// <param name="comp"></param>
-  [Pure]
   public static bool TryAddComp<T>(this ThingWithComps thingWithComps, T comp) where T : ThingComp
   {
     try
     {
-      thingWithComps.EnsureUncachedCompList();
+      CompList.Invoke(instance: thingWithComps) ??= [];
       thingWithComps.AllComps.Add(comp);
       return true;
     }
     catch (Exception ex)
     {
-      SmashLog.Error(
-        $"Exception thrown while trying to reflectively add <type>{comp.GetType()}</type> to {thingWithComps}.\nException={ex}");
+      Log.Error($"Exception thrown while trying to add {comp.GetType()} to {thingWithComps}." +
+                $"\nException={ex}");
     }
     return false;
   }
 
   /// <summary>
-  /// Assigns new list reference to comps field of <paramref name="thingWithComps"/>, rather than using an empty static list shared among all instances
+  /// Insert <paramref name="comp"/> to <paramref name="thingWithComps"/> and inits inner 'comps' list if empty.
   /// </summary>
-  private static void EnsureUncachedCompList(this ThingWithComps thingWithComps)
+  public static bool TryInsertComp<T>(this ThingWithComps thingWithComps, T comp, int index) where T : ThingComp
   {
     try
     {
       CompList.Invoke(instance: thingWithComps) ??= [];
+      thingWithComps.AllComps.Insert(index, comp);
+      return true;
     }
     catch (Exception ex)
     {
-      SmashLog.Error(
-        $"Exception thrown while trying to uncache compList for {thingWithComps}.\nException={ex}");
+      Log.Error($"Exception thrown while trying to reflectively add {comp.GetType()} to " +
+                $"{thingWithComps}.\nException={ex}");
     }
+    return false;
   }
 }
